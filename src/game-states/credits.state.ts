2155e639066cb133a7c22f89f27224ac510cdf00
mostofthings <62969747+mostofthings@users.noreply.github.com>
2021-09-04 15:52:41 -0500

    added and implemented credits.state.ts

--- src/game-states/credits.state.ts
+++ src/game-states/credits.state.ts
@@ -2,17 +2,27 @@ import { State } from "../core/state";
 import { assetEngine } from "../core/asset-engine-instance";
 import {controls} from "../core/controls";
 import {gameStateMachine} from "../game-state-machine";
+import {doTimes} from "../core/timing-helpers";
 
 
 class CreditsState implements State {
+  framesElapsed = 0
+
   onEnter() {
     assetEngine.musicEngine.startSong(4)
     controls.onClick(() => gameStateMachine.setState('menu'))
   }
 
   onUpdate() {
+    this.framesElapsed ++
     assetEngine.drawEngine.clearContext();
-    assetEngine.drawEngine.drawText('You Win!', 40,  120, 30);
+
+    const animationOffset = this.framesElapsed > 120 ? (this.framesElapsed - 120) / 4 : 0;
+    assetEngine.drawEngine.drawText('You Win!', 60, 120, 100 - animationOffset);
+    const credits = ['Robert Louison', 'James Schroederson', 'Jordan Louison', 'Nicole Witowskerson', 'Hendricks Henderson']
+    credits.forEach((text, index) => {
+      assetEngine.drawEngine.drawText(text, 40,  120, (index * 25) + 400 - animationOffset);
+    })
   }
 
   onLeave() {
